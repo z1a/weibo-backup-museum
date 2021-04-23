05bf38553999383b6a800f4c9fc1d8addd9ee13b
z1a <vizarovim@gmail.com>
2021-04-22 21:25:29 -0700

    Fix type error

--- src/backup/utils/delay.ts
+++ src/backup/utils/delay.ts
@@ -2,7 +2,7 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 /* Resolves after the given number of milliseconds. */
 export function delay(ms: number): Promise<void> {
-  return new Promise((res): number =>
+  return new Promise((res) =>
     setTimeout((): void => {
       res()
     }, ms)
